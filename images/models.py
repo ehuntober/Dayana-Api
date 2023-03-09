ce0b964cecb26f0f9ba6fd3ba01d45092226ff8f
ehuntober <ehuntercrypto@gmail.com>
2023-03-09 06:10:45 +0000

    models created

--- images/models.py
+++ images/models.py
@@ -4,3 +4,14 @@ from django.db import models
 from django.utils import timezone 
 from django.contrib.auth.models import User 
 
+def user_directory_path(instance,filename):
+    return 'images/{0}/'.format(filename)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Images(models.Model):
+    
