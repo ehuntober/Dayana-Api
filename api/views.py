0534e80d85d2cdc53499d8c4978b7958225130d9
ehuntober <ehuntercrypto@gmail.com>
2023-03-09 16:38:36 +0000

    new image added

--- api/views.py
+++ api/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
+# from rest_framework import generics
+# from api.custom_renderers import JPEGRenderer, PNGRenderer
+# from rest_framework.response import Response
+# from images.models import Images
+
+
+# class ImageAPIView(generics.RetrieveAPIView):
+    
+#     renderer_classes = [JPEGRenderer]
+#     def get(self,request,*args,**kwargs):
+    
+#         queryset = Images.objects.get(id=self.kwargs['id']).image
+#         data = queryset
+#         return Response(data,content_type='images/jpg')
+
+
+from django.shortcuts import render
 from rest_framework import generics
 from api.custom_renderers import JPEGRenderer, PNGRenderer
 from rest_framework.response import Response
 from images.models import Images
 
-
 class ImageAPIView(generics.RetrieveAPIView):
-    
     renderer_classes = [JPEGRenderer]
-    def get(self,request,*args,**kwargs):
-    
-        queryset = Images.objects.get(id=self.kwargs['id']).image
+
+    def get(self, request, *args, **kwargs):
+        queryset = Images.objects.get(id=self.kwargs['id'][:2]).image
         data = queryset
-        return Response(data,content_type='images/jpg')
+        return Response(data, content_type='image/jpeg')
+
