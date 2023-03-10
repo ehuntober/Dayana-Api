a7bac3cca981cb04c1a22e4fe31d690eb012119b
ehuntober <ehuntercrypto@gmail.com>
2023-03-10 17:00:37 +0000

    jwt installation down

--- dayanaEngine/settings.py
+++ dayanaEngine/settings.py
@@ -46,6 +46,12 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': [
+'rest_framework_simplejwt.authentication.JWTAuthentication',
+],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
