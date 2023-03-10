cd1bd0c81522d5da824e340b287677d05faa00be
ehuntober <ehuntercrypto@gmail.com>
2023-03-10 15:03:03 +0000

    creating serailizer for account app

--- dayanaEngine/urls.py
+++ dayanaEngine/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
-    path('',include('images.urls',namespace='images'))
+    path('',include('images.urls',namespace='images')),
+    path('auth/',include('accounts.urls'))
     
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
