1039ec37209eec8828ad6e2ae693cddaae0cb35c
ehuntober <ehuntercrypto@gmail.com>
2023-03-10 09:44:04 +0000

    created view for list of images

--- api/views.py
+++ api/views.py
@@ -1,14 +1,19 @@
 
 
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from api.custom_renderers import JPEGRenderer, PNGRenderer
 from rest_framework.response import Response
 from images.models import Images
 
 
 
-*
+# *** import  the apis 
+from rest_framework.response import Response 
+from rest_farmework.parsers import FileUploadParser 
+from django.shortcuts import get_objects_or_404 
+from .serializers import ImageSerializer
+
 
 class ImageAPIView(generics.RetrieveAPIView):
     renderer_classes = [JPEGRenderer]
@@ -17,4 +22,9 @@ class ImageAPIView(generics.RetrieveAPIView):
         queryset = Images.objects.get(id=self.kwargs['id'][:2]).image
         data = queryset
         return Response(data, content_type='image/jpeg')
+
+
+class ListImage(generics.ListAPIView):
+    queryset = Images.objects.all()
+    serializer_class = ImageSerializer
 
