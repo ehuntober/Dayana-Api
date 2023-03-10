b7df42da52594014c2027cf716f2b07a839c9cae
ehuntober <ehuntercrypto@gmail.com>
2023-03-10 16:02:17 +0000

    login user view created

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, status 
 from rest_framework.response import Response 
 from rest_framework_simplejwt.tokens import RefreshToken 
-from django.contrib.auth.models import User    
+from django.contrib.auth.models import User  
+from .serializers import UserSerializer  
 
 
 class RegisterAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+    
+    
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = User
