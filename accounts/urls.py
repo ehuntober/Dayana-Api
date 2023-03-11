9af3fd83411bdbba634485a9aef3fb49299b2a36
ehuntober <ehuntercrypto@gmail.com>
2023-03-11 00:17:31 +0000

    json web token added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 
-from django.urls import path  
+from django.urls import path 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+) 
 from .views import RegisterAPIView, LoginAPIView
 
 
 urlpatterns = [
     path('register/',RegisterAPIView.as_view(),name='register'),
-    path('login/',LoginAPIView.as_view(),name='login')
+    path('login/',LoginAPIView.as_view(),name='login'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
