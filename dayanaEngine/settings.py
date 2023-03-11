489d61568b08ed9940f646effe156b79d780dd93
ehuntober <ehuntercrypto@gmail.com>
2023-03-11 09:29:47 +0000

    static image added

--- dayanaEngine/settings.py
+++ dayanaEngine/settings.py
@@ -141,14 +141,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
--- images/urls.py
+++ images/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 # from django.views.generic import TemplateView
 from .views import ImagesListView , HomepageView
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 app_name = 'images' 
    
 urlpatterns = [
     # path('',TemplateView.as_view(template_name="images/index.html")),
     path('',HomepageView.as_view(),name="Homepage"),
     path('images/',ImagesListView.as_view(),name='images'),
+    
 ]
+
+urlpatterns += staticfiles_urlpatterns()
