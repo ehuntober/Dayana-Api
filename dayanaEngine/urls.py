957361ccc7f84fd8934bad63cd220c52378de433
ehuntober <ehuntercrypto@gmail.com>
2023-03-18 06:16:15 +0000

    modified dayana engine setting

--- dayanaEngine/urls.py
+++ dayanaEngine/urls.py
@@ -25,4 +25,3 @@ urlpatterns = [
     path('auth/',include('accounts.urls'))
     
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
- #url update
--- dayanaEngine/wsgi.py
+++ dayanaEngine/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dayanaEngine.settings')
 
 application = get_wsgi_application()
+ #url update
